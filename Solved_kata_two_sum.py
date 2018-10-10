539934c41c9cf904aa1755803bfd37460d3d8286
pskirank <pskiran.kundurthi@gmail.com>
2018-10-10 11:06:45 +0530

    Solved_Katas

--- Solved_kata_two_sum.py
+++ Solved_kata_two_sum.py
@@ -5,4 +5,4 @@ def two_sum(nums, t):
         for j, y in enumerate(nums):
             if i != j and x + y == t:
                 return [i, j]
-print two_sum([2,2,3], 4)
+print(two_sum([2,2,3], 4))
